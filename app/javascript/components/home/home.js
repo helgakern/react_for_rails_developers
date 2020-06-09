0c7b5e155fb2d1dcc5e352ea512ba6f2dacce14d
helgakern <helgakern@gmail.com>
2020-06-08 17:51:36 -0700

    updated gems

---
+++ app/javascript/components/home/home.js
@@ -0,0 +1,44 @@
+import React, {
+    Component
+} from 'react'
+
+class Home extends Component {
+    constructor() {
+        this.state = {
+            course_modules: [{
+                    id: 1,
+                    title: '1. Setting up a new Ruby on Rails App with React.',
+                    description: 'lorem ipsum',
+                    active: false
+                },
+                {
+                    id: 2,
+                    title: '2. Adding React to an Existing Rails App. ',
+                    description: 'lorem ipsum',
+                    active: false
+                },
+                {
+                    id: 3,
+                    title: '3. Building a Hello World App',
+                    description: 'lorem ipsum',
+                    active: false
+                },
+                {
+                    id: 4,
+                    title: '4. Adding React Router Dom to your App.',
+                    description: 'lorem ipsum',
+                    active: false
+                },
+
+
+            ]
+        }
+
+    }
+
+    render() {
+        return (
+
+        )
+    }
+}
